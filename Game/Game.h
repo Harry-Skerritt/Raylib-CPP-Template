a8cb663507b0be15fb2f2ca804ff98c23ce6f39b
Harry Skerritt <harry2.skerritt@live.uwe.ac.uk>
2026-07-12 22:16:12 +0100

    Create Game.h

---
+++ Game/Game.h
@@ -0,0 +1,23 @@
+//
+// Created by Harry Skerritt on 12/07/2026.
+//
+
+#ifndef GAME_H
+#define GAME_H
+
+class Game {
+public:
+    Game(const int window_width, const int window_height, const char* window_title, const int target_fps = 1000);
+    ~Game();
+
+    void init();
+    void update();
+    void render();
+
+    [[nodiscard]] bool isRunning() const { return m_is_running; }
+
+private:
+    bool m_is_running = true;
+};
+
+#endif //GAME_H
