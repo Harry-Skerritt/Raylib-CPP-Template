f7812509679f604539d219afc0e8da976f5039e0
Harry Skerritt <harry2.skerritt@live.uwe.ac.uk>
2026-07-13 20:19:37 +0100

    Updated SceneManager

--- src/Managers/SceneManager/SceneManager.cpp
+++ src/Managers/SceneManager/SceneManager.cpp
@@ -11,14 +11,24 @@ void SceneManager::setScene(std::unique_ptr<Scene> new_scene, TransitionType typ
 
     if (type == TransitionType::SNAP) {
         m_active_scene = std::move(m_next_scene);
-    } else {
+    }
+    else if (type == TransitionType::FADE_TO_BLACK || type == TransitionType::FADE_TO_WHITE) {
         m_state = ManagerState::FADING_OUT;
         m_fade_alpha = 0.0f;
+    }
+    else if (type == TransitionType::CROSS_FADE) {
+        m_state = ManagerState::CROSSFADING;
+        m_fade_alpha = 0.0f;
+
+        if (m_transition_target.id == 0) {
+            m_transition_target = LoadRenderTexture(GetScreenWidth(), GetScreenHeight());
+        }
     }
 }
 
 void SceneManager::update(const float dt) {
     handleFadeUpdate(dt);
+    handleCrossFadeUpdate(dt);
 
     if (m_active_scene && m_state != ManagerState::FADING_OUT) {
         m_active_scene->update(dt);
@@ -26,17 +36,27 @@ void SceneManager::update(const float dt) {
 }
 
 void SceneManager::render() const {
-    if (m_active_scene) m_active_scene->render();
+    if (m_state == ManagerState::CROSSFADING) {
+        drawFadedTexture([](Scene* s) { s->render(); });
+    }
+    else
+        {
+        if (m_active_scene) m_active_scene->render();
 
-    if (m_state != ManagerState::IDLE) {
-        DrawRectangle(0, 0, GetScreenWidth(), GetScreenHeight(), Fade(BLACK, m_fade_alpha));
+        if (m_state != ManagerState::IDLE) {
+            const Color fade_color = (m_transition_type == TransitionType::FADE_TO_WHITE) ? WHITE : BLACK;
+            DrawRectangle(0, 0, GetScreenWidth(), GetScreenHeight(), Fade(fade_color, m_fade_alpha));
+        }
     }
 }
 
 void SceneManager::renderUI() const {
-    if (m_active_scene) m_active_scene->renderUI();
+    if (m_state == ManagerState::CROSSFADING) {
+        drawFadedTexture([](Scene* s) { s->renderUI(); });
+    } else {
+        if (m_active_scene) m_active_scene->renderUI();
+    }
 }
-
 
 
 // Private
@@ -55,6 +75,30 @@ void SceneManager::handleFadeUpdate(const float dt) {
             m_state = ManagerState::IDLE;
         }
     }
+}
+
+// Crossfade
+void SceneManager::handleCrossFadeUpdate(const float dt) {
+    if (m_state == ManagerState::CROSSFADING) {
+        m_fade_alpha += dt * m_fade_speed;
+        if (m_fade_alpha >= 1.0f) {
+            m_fade_alpha = 1.0f;
+            m_active_scene = std::move(m_next_scene);
+            m_state = ManagerState::IDLE;
+        }
+    }
+}
+
+void SceneManager::drawFadedTexture(const std::function<void(Scene *)> &renderFunc) const {
+    if (m_active_scene) renderFunc(m_active_scene.get());
+
+    BeginTextureMode(m_transition_target);
+    ClearBackground(BLANK);
+    if (m_next_scene) renderFunc(m_next_scene.get());
+    EndTextureMode();
+
+    Rectangle src = { 0, 0, (float)m_transition_target.texture.width, -(float)m_transition_target.texture.height };
+    DrawTextureRec(m_transition_target.texture, src, { 0, 0 }, Fade(WHITE, m_fade_alpha));
 }
 
 
--- src/Managers/SceneManager/SceneManager.h
+++ src/Managers/SceneManager/SceneManager.h
@@ -6,12 +6,24 @@
 #define SCENEMANAGER_H
 #include <memory>
 #include "Scene.h"
+#include <raylib.h>
+#include <functional>
 
-enum class TransitionType { SNAP, FADE };
-enum class ManagerState { IDLE, FADING_IN, FADING_OUT };
+enum class TransitionType {
+    SNAP,
+    FADE_TO_BLACK,
+    FADE_TO_WHITE,
+    CROSS_FADE
+};
+
+enum class ManagerState { IDLE, FADING_IN, FADING_OUT, CROSSFADING };
 
 class SceneManager {
 public:
+    SceneManager() = default;
+    ~SceneManager() {
+        if (m_transition_target.id != 0) UnloadRenderTexture(m_transition_target);
+    }
     void setScene(std::unique_ptr<Scene> new_scene, TransitionType type = TransitionType::SNAP);
     void update(float dt);
     void render() const;
@@ -25,7 +37,13 @@ private:
     TransitionType m_transition_type = TransitionType::SNAP;
     float m_fade_alpha = 0.0f;
     const float m_fade_speed = 2.0f;
-    void handleFadeUpdate(float dt);
-};
 
+    // Fade To Black
+    void handleFadeUpdate(float dt);
+
+    // Crossfade
+    RenderTexture2D m_transition_target = { 0 };
+    void handleCrossFadeUpdate(float dt);
+    void drawFadedTexture(const std::function<void(Scene*)>& renderFunc) const;
+};
 #endif //SCENEMANAGER_H
